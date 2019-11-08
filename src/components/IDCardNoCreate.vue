7a28ca28fadba7b987bc015850717a0ae323e142
wangqiang <wangqiang@keking.cn>
2019-11-08 15:17:18 +0800

    first commit

---
+++ src/components/IDCardNoCreate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="idcardcrreate">
+      新建身份证号码
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "idcardcrreate"
+    }
+</script>
+
+<style scoped>
+
+</style>
